72f5448ad5c6da2680a8de9f5b971b0fcc977c92
helenaguertin <helena.guertin@etu.univ-poitiers.fr>
2026-09-21 15:08:27 +0000

    feat: ✨ implement frontend

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from 'react';
+import type { Movie } from '../back-end/schemas/MoviesTypes';
+import MovieItem from './components/MovieItem';
+
 export default function App() {
+  useEffect(() => {
+    // fetch data from an API /api/movies/popular
+    fetch('/api/movies/popular')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  const [movies, setMovies] = useState<Movie[] | null>(null);
+
+  // useEffect hook to fetch data from an API when the component mounts
+  useEffect(() => {
+    // fetch data from an API /api/movies/popular
+    fetch('/api/movies/popular')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Fetched movies data:', data); // Log the fetched data for debugging
+        setMovies(data.results); // Update the state with the fetched movies data
+      });
+  }, []);
+
   return (
     <div>
-      Hello Vite from React!
+      <h1>Popular Movies</h1>
+      {movies ? (
+        <ul>
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} movie={movie} />
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  )
+  );
 }
