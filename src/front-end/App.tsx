13d06a62895c4c18a324faf7ee9dfb1ea8f168d6
helenaguertin <helena.guertin@etu.univ-poitiers.fr>
2026-09-23 15:56:34 +0000

    style: 🎨 apply movie page classes

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -26,8 +26,8 @@ export default function App() {
   }, []);
 
   return (
-    <main>
-      <header>
+    <main className="app-shell">
+      <header className="app-header">
         <h1>Films populaires</h1>
         <h2>
           Films tendances en France, d'après les données de{' '}
@@ -36,7 +36,7 @@ export default function App() {
       </header>
       <section>
         {movies ? (
-          <ul>
+          <ul className="movie-grid">
             {movies.map((movie) => (
               <li key={movie.id}>
                 <article>
@@ -46,7 +46,7 @@ export default function App() {
             ))}
           </ul>
         ) : (
-          <p>Loading...</p>
+          <p className="status-message">Loading...</p>
         )}
       </section>
     </main>
